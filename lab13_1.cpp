76721624ed663a880d7a152ca9469d66201151fc
NATKAMONLAK <paintlpch@gmail.com>
2021-01-22 16:13:49 +0700

    insertion sort

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,11 +3,29 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i,j;
+	for(i=1;i<N;i++){
+		T temp = d[i];
+		j = i-1;
 
+		while(j>=0 && d[j] < temp){
+			d[j+1] = d[j];
+			j--;
+		}
+		d[j+1] = temp;
+
+
+		cout<<"Pass "<<i<<":";
+		for (int m = 0; m < N; m++){
+			cout << d[m] << " ";  
+		}  
+    	cout << endl;
+	}
 }
 
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	double a[10] = {69.9 ,0.25, 0.5, 1.34, 2.1, 0, 4.8, 88.36, 11.01 ,41.25};
+	//int a[10] = {12 ,25, 30, 44, 2, 0, 4, 7, 55, 25};
 	cout << "Input Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
